9e526071b58d6bdae4dde0192cf63120f0aaa9bd
JoeNatan30 <huamani.jn@pucp.edu.pe>
2022-07-04 10:24:10 -0500

    add empty keypointsestimator functions

---
+++ keypointEstimators/mediapipe_functions.py
@@ -0,0 +1,17 @@
+# Standard library imports
+
+# Third party imports
+import mediapipe as mp4
+import cv2
+
+# Local imports
+
+def model_init():
+    print("init")
+
+def frame_process():
+
+    print("frame_process")
+
+model_init()
+frame_process()
