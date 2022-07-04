f2874931e6ad2c985a2e1c46c7a5aac1793f9e91
JoeNatan30 <huamani.jn@pucp.edu.pe>
2022-07-04 14:13:24 -0500

    output using numpy

--- keypointEstimators/mediapipe_functions.py
+++ keypointEstimators/mediapipe_functions.py
@@ -20,7 +20,7 @@ def model_init(static_image_mode = True, ):
     print("init MediaPipe model - Done")
     return holistic
 
-def frame_process(frame):
+def frame_process(holistic, frame):
 
     imageBGR = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     height, width, _ = imageBGR.shape
@@ -36,6 +36,7 @@ def frame_process(frame):
         kpDict["pose"] = [ [point.x, point.y] for point in holisResults.pose_landmarks.landmark]
     else:
         kpDict["pose"] = [ [0.0, 0.0] for point in range(0, 33)]
+    kpDict["pose"] = np.asarray(kpDict["pose"])
 
     # HANDS
 
@@ -46,6 +47,7 @@ def frame_process(frame):
         kpDict["left_hand"] = [ [point.x, point.y] for point in holisResults.left_hand_landmarks.landmark]
     else:
         kpDict["left_hand"] = [ [0.0, 0.0] for point in range(0, 21)]
+    kpDict["left_hand"] = np.asarray(kpDict["left_hand"])
 
     # Right hand
     kpDict["right_hand"]={}
@@ -55,6 +57,7 @@ def frame_process(frame):
 
     else:
         kpDict["right_hand"] = [ [0.0, 0.0] for point in range(0, 21)]
+    kpDict["right_hand"] = np.asarray(kpDict["right_hand"])
 
     # Face mesh
     kpDict["face"]={}
@@ -65,17 +68,17 @@ def frame_process(frame):
 
     else:
         kpDict["face"] = [[0.0, 0.0] for point in range(0, 468)]
+    kpDict["face"] = np.asarray(kpDict["face"])
 
     print("frame_process")
     return kpDict
 
 '''
-holistic = model_init()
+model = model_init()
 cap = cv2.VideoCapture(os.getcwd()+'/datasets/???_18.mp4')
 ret, frame = cap.read()
 
-#frame = np.random.rand(100,100,3)
-frame_kp = frame_process(frame)
+frame_kp = frame_process(model, frame)
 print(frame_kp)
-holistic.close()
+model.close()
 '''
