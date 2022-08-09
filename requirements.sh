daf6b2fe93cc829e719615478f2f31be4b7ecfcf
JoeNatan30 <huamani.jn@pucp.edu.pe>
2022-08-08 20:15:45 -0500

    requiremets

---
+++ requirements.sh
@@ -0,0 +1,11 @@
+conda create --name signs python=3.8
+conda activate signs
+pip install --upgrade pip
+pip install youtube-dl
+pip install opencv-python
+pip install pandas
+pip install pytorch-lightning
+conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
+pip install mediapipe
+conda install -c conda-forge yacs
+pip install gdown
