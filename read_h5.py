928ef6eca9ef8123ab59dccf05a8f9c5a8573104
JoeNatan30 <huamani.jn@pucp.edu.pe>
2022-08-20 11:28:39 -0500

    to read h5 output

--- read_h5.py
+++ read_h5.py
@@ -1,9 +1,7 @@
-import os
 import pandas as pd
 import numpy as np
 import h5py
-from torchvision.io import read_image
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 
 class H5Dataset(Dataset):
     def __init__(self, filePath):
