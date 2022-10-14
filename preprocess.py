edeb8726d8854a92fcdbfaa8df0ab6a9bd2485ac
JoeNatan30 <huamani.jn@pucp.edu.pe>
2022-10-13 23:14:22 -0500

    left pickle, now H5

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,6 @@
 # Standard library imports
 from multiprocessing import Pool
-from itertools import repeat
+#from itertools import repeat
 import os
 import warnings
 
@@ -9,10 +9,11 @@ import pandas as pd
 import numpy as np
 import cv2
 import time
+import h5py
 
 # Local imports
 import commandSystem as cs
-from datasetVideoReader import AEC_videoReader, PUCP_videoReader, WASL_videoReader
+from datasetVideoReader import AEC_videoReader, PUCP_videoReader, WASL_videoReader, PUCP_PSL_DGI305_videoReader
 
 try:
     from keypointEstimators import mediapipe_functions
@@ -39,6 +40,7 @@ def get_datasets_data(dataset_opt):
 
     video_reader = {"AEC": AEC_videoReader,
                     "PUCP_PSL_DGI156": PUCP_videoReader,
+                    "PUCP_PSL_DGI305": PUCP_PSL_DGI305_videoReader,
                     "WLASL": WASL_videoReader}
 
     dataset_list = []
@@ -81,13 +83,13 @@ def get_keypoint_from_estimator(path, label, model, kp_est_chosed, keypoint_esti
 
         ret, frame = cap.read()
 
-    results = [v for k, v in results.items() if v != []]
-    results.append(label)
-    results.append(path.split(os.sep)[-1])
+    results = [np.array(v) for k, v in results.items() if v != []]
+    #results.append(label)
+    #results.append(path.split(os.sep)[-1])
     et = time.time()
     elapsed_time = et - st
     print('Execution time:', elapsed_time, 'seconds')
-    return results
+    return np.squeeze(np.array(results), axis=0)
 
 def partial_save(output, partial_output_name, estimator):
 
@@ -107,40 +109,33 @@ def get_keypoint_estimator_standarized_output(kpoint_est_opt, data, partial_outp
         keypoint_estimator = mediapipe_functions
     elif "wholepose" == kp_est_chosed:
         keypoint_estimator = wholepose_functions 
-    
+
     # initilize the model
     model = keypoint_estimator.model_init()
  
-    # data acumulators
-    kp_acum = []
-    path_acum = []
-    name_acum = []
-    label_acum = []
-
     num = 0
+
+    save_path = f"./output/{partial_output_name}--{kp_est_chosed}.hdf5"
+
+    h5_file = h5py.File(save_path, 'w')
 
     # For each video (path)
     for path, label in zip(data['video_path'], data['label']):
 
         # Where the video is processed
         kp_list = get_keypoint_from_estimator(path, label, model, kp_est_chosed, keypoint_estimator)
-        
+ 
+        # accumulate data
+        grupo_name = f"{num}"
+        h5_file.create_group(grupo_name)
+        h5_file[grupo_name]['video_name'] = os.sep.join(path.split(os.sep)[-2:])
+        h5_file[grupo_name]['label'] = label
+        h5_file[grupo_name]['data'] = np.asarray(kp_list)
+
         num = num + 1
         print(f"Video processed: {num}")
         
-        # accumulate data
-        kp_acum.append(kp_list)
-        label_acum.append(label)
-        name_acum.append(path.split(os.sep)[-1])
-
-        # Format of the output
-        output = pd.DataFrame.from_dict({
-            "data":  kp_acum,
-            "label": label_acum,
-            "name":  name_acum,
-        }, orient='index')
-
-        partial_save(output, partial_output_name, kp_est_chosed)
+    h5_file.close()
 
     # close the model (some models no need to close)
     keypoint_estimator.close_model(model)
